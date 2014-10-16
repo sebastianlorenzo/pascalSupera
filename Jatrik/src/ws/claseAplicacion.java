7127ce657da300422009283291d37d7b1f36f439
patriciar <rolandipat@gmail.com>
2014-10-16 15:52:30 -0200

    nueva clase aplicacion

--- Jatrik/src/ws/claseAplicacion.java
+++ Jatrik/src/ws/claseAplicacion.java
@@ -4,6 +4,6 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("rest")
-public class claseAplicacion extends Application {
-    
+public class claseAplicacion extends Application{
+
 }
